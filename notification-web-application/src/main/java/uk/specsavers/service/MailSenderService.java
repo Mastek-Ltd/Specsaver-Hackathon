21355049ae42da189c6b4cf141faffd693dbfd23
'krishnak' <krishna.kuntala@mastek.com>
2015-08-28 20:51:04 +0100

    Added multiple implementations.

--- notification-web-application/src/main/java/uk/specsavers/service/MailSenderService.java
+++ notification-web-application/src/main/java/uk/specsavers/service/MailSenderService.java
@@ -1,9 +1,14 @@
 package uk.specsavers.service;
 
+import java.io.IOException;
+
+import javax.activation.FileTypeMap;
+import javax.activation.MimetypesFileTypeMap;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -14,14 +19,33 @@ public class MailSenderService {
 	@Autowired
 	private JavaMailSenderImpl mailSender;
 
-	public void sendEmail() throws MessagingException {
+	@Value("${specsavers.email.default.subject}")
+	private String defaultEmailSubject;
+
+	@Value("${specsavers.email.default.toAddress}")
+	private String defaultEmailToAddress;
+
+	@Value("${specsavers.email.default.messageText}")
+	private String defaultEmailMessageText;
+
+	public void sendEmail(String subject, String toAddress, String messageText) throws MessagingException {
 		MimeMessage message = mailSender.createMimeMessage();
 		MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
-		messageHelper.setSubject("Dummy Subject");
-		messageHelper.setTo("specsavers.hackathon@gmail.com");
-		messageHelper.setText("Test message!!!", true);
-
+		messageHelper.setSubject(subject);
+		messageHelper.setTo(toAddress);
+		messageHelper.setText(messageText, true);
 		mailSender.send(message);
+	}
 
+	public void sendEmail(String toAddress, String messageText) throws MessagingException {
+		this.sendEmail(defaultEmailSubject, toAddress, messageText);
+	}
+
+	public void sendEmail(String toAddress) throws MessagingException {
+		this.sendEmail(toAddress, defaultEmailMessageText);
+	}
+
+	public void sendEmail() throws MessagingException {
+		this.sendEmail(defaultEmailToAddress);
 	}
 }
